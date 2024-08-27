613e2a9b53cfd3420eccd1a58dc810f5a4344b9b
Cesar Barbosa Gonçalves <cesarbgoncalves@gmail.com>
2024-08-27 13:45:14 -0300

    Melhorando script de entrypoint

--- run.sh
+++ run.sh
@@ -4,19 +4,22 @@
 ENV_FILE="/main/.env"
 
 # Definindo a conexão como MySQL
-echo 'DB_CONNECTION=mysql' >> "$ENV_FILE"
+echo 'DB_CONNECTION=mysql' > "$ENV_FILE"
 
 # Definindo as variáveis de conexão com o BD a partir das variáveis em  tempo de execução do container.
 for var in $(env | cut -d= -f1| grep -e ^DB); do
     echo "$var=$(printenv $var)" >> "$ENV_FILE"
 done
+# Acessando o diretório do projeto
+cd /main
 
 # Verificando se a migração já foi realizada
-if php artisan migrate:status | grep -q 'Migration table not found'; then
-echo "Migrações não aplicadas. Executando php artisan migrate..."
-php artisan migrate --seed
+php artisan migrate:status | grep -q ERROR
+if [ $? -eq 0 ]; then
+    echo "Migrações não aplicadas. Executando php artisan migrate..."
+    php artisan migrate --seed --force
 else
-echo "Migrações já aplicadas."
+    echo "Migrações já aplicadas."
 fi
 
 # Rodando o comando para iniciar o servidor
